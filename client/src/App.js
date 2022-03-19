f91e5a5844b3f3e21f04fd8e470fa1fe7f076fd2
Paul N Barnes <paulieb99@gmail.com>
2022-03-19 16:02:58 -0700

    created products, header, footer for dummy data. Mostly working on the front end first to see visual when working with back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductScreen from './pages/ProductScreen';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to MERNY's</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/product/:id' element={ProductScreen} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
