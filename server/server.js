7675ece1378c2efae71e21e942875b3db75bc282
Paul N Barnes <paulieb99@gmail.com>
2022-03-19 17:14:08 -0700

    connected front end to fetch products and single product from back end with useState useEffect and axios

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const products = require('./data/products');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
