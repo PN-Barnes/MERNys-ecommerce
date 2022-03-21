1f0366b10c2cb9db4a65e8a0851e9d345bdcbe04
Paul N Barnes <paulieb99@gmail.com>
2022-03-21 15:49:11 -0700

    created the models for users, products, and orders

---
+++ server/models/productModel.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const reviewSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: String, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+const productSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Reviews = mongoose.model('Reviews', reviewSchema);
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = { Product, Reviews };
