c604603fc14834ba04ad5637fa6026698a8f2f88
Paul N Barnes <paulieb99@gmail.com>
2022-03-29 22:03:35 -0700

    added constant, reducer, and action for add to cart and brought that into the cart page to add to state and see it with redux tools

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import { Link, useParams, useLocation, useHistory } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { addToCart } from '../actions/cartActions';
 
-const CartPage = () => {
+const CartPage = ({ location, history }) => {
+  const { id: productId } = useParams();
+
+  const qty = new URLSearchParams(useLocation().search).get('qty');
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  console.log(cartItems);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
   return <h1>Cart</h1>;
 };
 
--- client/src/pages/ProductScreen.js
+++ client/src/pages/ProductScreen.js
@@ -19,7 +19,7 @@ import { listProductsDetails } from '../actions/productActions';
 const ProductScreen = (history) => {
   const match = useParams();
   const navigate = useNavigate();
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
 
