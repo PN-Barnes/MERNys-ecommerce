ad299948a19a74ddee97e6b4f74df8944a8fd76e
Paul N Barnes <paulieb99@gmail.com>
2022-03-31 22:16:46 -0700

    had to import useNavigate to replace the history.push in order to have proceed to checkout button redirect to new page if logged in, switched back form control and left the incremental out as comments in order to test later on

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
-import { Link, useParams, useLocation, useHistory } from 'react-router-dom';
+import { Link, useParams, useLocation, useNavigate } from 'react-router-dom';
 import {
   Row,
   Col,
@@ -14,8 +14,8 @@ import {
 import { addToCart } from '../actions/cartActions';
 
 const CartPage = ({ location, history }) => {
+  const navigate = useNavigate();
   const { id: productId } = useParams();
-
   const qty = new URLSearchParams(useLocation().search).get('qty');
 
   const dispatch = useDispatch();
@@ -28,21 +28,29 @@ const CartPage = ({ location, history }) => {
       dispatch(addToCart(productId, qty));
     }
   }, [dispatch, productId, qty]);
-
-  const updateQtyHandler = (id, qty, action) => {
-    if (action === 'INC') {
-      dispatch(addToCart(id, qty + 1));
-    }
-    if (action === 'DEC') {
-      dispatch(addToCart(id, qty - 1));
-    }
-  };
+  // const updateQtyHandler = (id,qty,action) =>{
+  // if(action === 'INC'){
+  //     dispatch(addToCart( id , qty + 1))
+  // }
+  // if(action === 'DEC'){
+  //  dispatch(addToCart(id , qty - 1))
+  // }
+  // }
+  // <Button type='button'  variant="dark" size='sm'onClick ={() => updateQtyHandler(item.product,item.qty,'INC')}> + </Button>
+  // <input type="text" value={item.qty}  id='qty'size="1" />
+  // <Button  type='button'   variant='dark' size='sm' onClick ={() => updateQtyHandler(item.product,item.qty,'DEC')}> - </Button>
   const removeFromCart = (id) => {
     console.log('remove');
   };
+
+  const checkOutHandler = () => {
+    console.log('checkout');
+
+    navigate('/login?redirect=shipping');
+  };
   return (
     <Row>
-      <Col md={9}>
+      <Col md={8}>
         <h1>Shopping Cart</h1>
         {cartItems.length === 0 ? (
           <Message>
@@ -53,46 +61,28 @@ const CartPage = ({ location, history }) => {
             {cartItems.map((item) => (
               <ListGroup.Item key={item.product}>
                 <Row>
-                  <Col md={2}>
+                  <Col>
                     <Image src={item.image} alt={item.name} fluid rounded />
                   </Col>
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                   </Col>
-                  <Col md={2} className='text-center'>
-                    <Button
-                      type='button'
-                      variant='dark'
-                      className='mx-1'
-                      size='sm'
-                      onClick={() =>
-                        updateQtyHandler(item.product, item.qty, 'DEC')
-                      }
-                    >
-                      <i className='fas fa-minus'></i>
-                    </Button>
-
-                    <p
-                      type='text'
-                      className='mx-1 text-center'
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
                       value={item.qty}
-                      id='qty'
-                      size='1'
-                    >
-                      {item.qty}
-                    </p>
-
-                    <Button
-                      type='button'
-                      className='mx-1'
-                      variant='dark'
-                      size='sm'
-                      onClick={() =>
-                        updateQtyHandler(item.product, item.qty, 'INC')
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
                       }
                     >
-                      <i className='fas fa-plus '></i>
-                    </Button>
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
                   </Col>
                   <Col md={2} className='text-center'>
                     <Button
@@ -109,18 +99,28 @@ const CartPage = ({ location, history }) => {
           </ListGroup>
         )}
       </Col>
-      <Col md={3}>
+      <Col md={4}>
         <Card>
           <ListGroup variant='flush'>
             <ListGroup.Item>
               <h2>
-                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)} )
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
                 Items
               </h2>
               $
               {cartItems
                 .reduce((acc, item) => acc + item.qty * item.price, 0)
                 .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disable={cartItems.length === 0 ? true : 0}
+                onClick={checkOutHandler}
+              >
+                Proceed to checkout
+              </Button>
             </ListGroup.Item>
           </ListGroup>
         </Card>
